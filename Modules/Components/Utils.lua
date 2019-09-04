cb9f45aeff397557eed0519ce75e0a0b87b01a97
MisterCrab <51509374+MisterCrab@users.noreply.github.com>
2019-09-04 17:34:45 +0300

    cvar print fix

--- Modules/Components/Utils.lua
+++ Modules/Components/Utils.lua
@@ -437,9 +437,7 @@ local function UpdateCVAR()
 	local nameplateMaxDistance = GetCVar("nameplateMaxDistance")
     if nameplateMaxDistance and nameplateMaxDistance ~= toStr[ACTION_CONST_CACHE_DEFAULT_NAMEPLATE_MAX_DISTANCE] then 
 		SetCVar("nameplateMaxDistance", ACTION_CONST_CACHE_DEFAULT_NAMEPLATE_MAX_DISTANCE) 
-		if isCheckedOnce then 
-			A.Print("nameplateMaxDistance " .. nameplateMaxDistance .. " => " .. ACTION_CONST_CACHE_DEFAULT_NAMEPLATE_MAX_DISTANCE)	
-		end 
+		A.Print("nameplateMaxDistance " .. nameplateMaxDistance .. " => " .. ACTION_CONST_CACHE_DEFAULT_NAMEPLATE_MAX_DISTANCE)	
 	end		
 	
     -- WM removal
