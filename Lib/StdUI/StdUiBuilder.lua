a92286ff056ede79c79e6364bf17058d8b7125e3
MisterCrab <51509374+MisterCrab@users.noreply.github.com>
2019-09-22 16:38:48 +0300

    Update Lib

---
+++ Lib/StdUI/StdUiBuilder.lua
@@ -0,0 +1,259 @@
+--- @type StdUi
+local StdUi = LibStub and LibStub('StdUi', true);
+if not StdUi then
+	return;
+end
+
+local module, version = 'Builder', 4;
+if not StdUi:UpgradeNeeded(module, version) then return end;
+
+
+local function __genOrderedIndex(t)
+	local orderedIndex = {};
+
+	for key in pairs(t) do
+		tinsert(orderedIndex, key)
+	end
+
+	table.sort(orderedIndex, function (a, b)
+		if not t[a].order or not t[b].order then
+			return a < b;
+		end
+		return t[a].order < t[b].order;
+	end);
+
+	return orderedIndex;
+end
+
+local function orderedNext(t, state)
+	local key;
+
+	if state == nil then
+		-- the first time, generate the index
+		t.__orderedIndex = __genOrderedIndex(t);
+		key = t.__orderedIndex[1];
+	else
+		-- fetch the next value
+		for i = 1, table.getn(t.__orderedIndex) do
+			if t.__orderedIndex[i] == state then
+				key = t.__orderedIndex[i + 1];
+			end
+		end
+	end
+
+	if key then
+		return key, t[key];
+	end
+
+	-- no more value to return, cleanup
+	t.__orderedIndex = nil;
+	return
+end
+
+local function orderedPairs(t)
+	return orderedNext, t, nil;
+end
+
+local function setDatabaseValue(db, key, value)
+	if key:find('.') then
+		local accessor = StdUi.Util.stringSplit('.', key);
+		local startPos = db;
+
+		for i, subKey in pairs(accessor) do
+			if i == #accessor then
+				startPos[subKey] = value;
+				return;
+			end
+
+			startPos = db[subKey];
+		end
+	else
+		db[key] = value;
+	end
+end
+
+local function getDatabaseValue(db, key)
+	if key:find('.') then
+		local accessor = StdUi.Util.stringSplit('.', key);
+		local startPos = db;
+
+		for i, subKey in pairs(accessor) do
+			if i == #accessor then
+				return startPos[subKey];
+			end
+
+			startPos = db[subKey];
+		end
+	else
+		return db[key];
+	end
+end
+
+---BuildElement
+---@param frame Frame
+---@param row EasyLayoutRow
+---@param info table
+---@param dataKey string
+---@param db table
+function StdUi:BuildElement(frame, row, info, dataKey, db)
+	local element;
+
+	local genericChangeEvent = function(el, value)
+		setDatabaseValue(el.dbReference, el.dataKey, value);
+		if info.onChange then
+			info.onChange(el, value);
+		end
+	end
+
+	local hasLabel = false;
+	if info.type == 'checkbox' then
+		element = self:Checkbox(frame, info.label);
+		element.dbReference = db;
+		element.dataKey = dataKey;
+
+		if db then
+			element:SetChecked(getDatabaseValue(db, dataKey));
+			element.OnValueChanged = genericChangeEvent;
+		end
+	elseif info.type == 'text' or info.type == 'editBox' then
+		element = self:EditBox(frame, nil, 20);
+		element.dbReference = db;
+		element.dataKey = dataKey;
+
+		if info.label then
+			self:AddLabel(frame, element, info.label);
+			hasLabel = true;
+		end
+
+		if db then
+			element:SetValue(getDatabaseValue(db, dataKey));
+			element.OnValueChanged = genericChangeEvent;
+		end
+	elseif info.type == 'button' then
+		element = self:Button(frame, nil, 20, info.text or '');
+
+		if info.onClick then
+			element:SetScript('OnClick', info.onClick);
+		end
+	elseif info.type == 'dropdown' then
+		element = self:Dropdown(frame, 300, 20, info.options or {}, nil, info.multi or nil, info.assoc or false);
+		element.dbReference = db;
+		element.dataKey = dataKey;
+
+		if info.label then
+			self:AddLabel(frame, element, info.label);
+			hasLabel = true;
+		end
+
+		if db then
+			element:SetValue(getDatabaseValue(db, dataKey));
+			element.OnValueChanged = genericChangeEvent;
+		end
+	elseif info.type == 'autocomplete' then
+		element = self:Autocomplete(frame, 300, 20, '');
+		if info.validator then
+			element.validator = info.validator;
+		end
+		if info.transformer then
+			element.transformer = info.transformer;
+		end
+		if info.buttonCreate then
+			element.buttonCreate = info.buttonCreate;
+		end
+		if info.buttonUpdate then
+			element.buttonUpdate = info.buttonUpdate;
+		end
+		if info.items then
+			element:SetItems(info.items);
+		end
+
+		element.dbReference = db;
+		element.dataKey = dataKey;
+
+		if info.label then
+			self:AddLabel(frame, element, info.label);
+			hasLabel = true;
+		end
+
+		if db then
+			element:SetValue(getDatabaseValue(db, dataKey));
+			element.OnValueChanged = genericChangeEvent;
+		end
+	elseif info.type == 'sliderWithBox' then
+		element = self:SliderWithBox(frame, nil, 32, 0, info.min or 0, info.max or 2);
+		element.dbReference = db;
+		element.dataKey = dataKey;
+
+		if info.label then
+			self:AddLabel(frame, element, info.label);
+			hasLabel = true;
+		end
+
+		if info.precision then
+			element:SetPrecision(info.precision);
+		end
+
+		if db then
+			element:SetValue(getDatabaseValue(db, dataKey));
+			element.OnValueChanged = genericChangeEvent;
+		end
+	elseif info.type == 'header' then
+		element = StdUi:Header(frame, info.label);
+	elseif info.type == 'custom' then
+		element = info.createFunction(frame, row, info, dataKey, db);
+	end
+
+	if element.hasLabel then
+		hasLabel = true;
+	end
+
+	row:AddElement(element, {
+		column = info.column or 12,
+		margin = info.layoutMargins or {
+			top = (hasLabel and 20 or 0)
+		}
+	});
+
+	return element;
+end
+
+---BuildRow
+---@param frame Frame
+---@param info table
+---@param db table
+function StdUi:BuildRow(frame, info, db)
+	local row = frame:AddRow();
+
+	for key, element in orderedPairs(info) do
+		local dataKey = element.key or key or nil;
+
+		local el = self:BuildElement(frame, row, element, dataKey, db);
+		if element then
+			if not frame.elements then
+				frame.elements = {};
+			end
+
+			frame.elements[key] = el;
+		end
+	end
+end
+
+---BuildWindow
+---@param frame Frame
+---@param info table
+function StdUi:BuildWindow(frame, info)
+	local db = info.database or nil;
+
+	assert(info.rows, 'Rows are required in order to build table');
+	local rows = info.rows;
+
+	self:EasyLayout(frame, info.layoutConfig);
+
+	for i, row in orderedPairs(rows) do
+		self:BuildRow(frame, row, db);
+	end
+
+	frame:DoLayout();
+end
+
+StdUi:RegisterModule(module, version);
---
+++ Lib/StdUI/widgets/Dropdown.lua
@@ -0,0 +1,248 @@
+--- @type StdUi
+local StdUi = LibStub and LibStub('StdUi', true);
+if not StdUi then
+	return;
+end
+
+local module, version = 'Dropdown', 2;
+if not StdUi:UpgradeNeeded(module, version) then return end;
+
+-- reference to all other dropdowns to close them when new one opens
+local dropdowns = {};
+
+--- Creates a single level dropdown menu
+--- local options = {
+---		{text = 'some text', value = 10},
+---		{text = 'some text2', value = 11},
+---		{text = 'some text3', value = 12},
+--- }
+function StdUi:Dropdown(parent, width, height, options, value, multi, assoc)
+	local this = self;
+	local dropdown = self:Button(parent, width, height, '');
+	dropdown.text:SetJustifyH('LEFT');
+	-- make it shorter because of arrow
+	dropdown.text:ClearAllPoints();
+	self:GlueAcross(dropdown.text, dropdown, 2, -2, -16, 2);
+	
+	local dropTex = self:Texture(dropdown, 15, 15, [[Interface\Buttons\SquareButtonTextures]]);
+	dropTex:SetTexCoord(0.45312500, 0.64062500, 0.20312500, 0.01562500);
+	self:GlueRight(dropTex, dropdown, -2, 0, true);
+
+	local optsFrame = self:FauxScrollFrame(dropdown, dropdown:GetWidth(), 200, 10, 20);
+	optsFrame:Hide();
+	self:GlueBelow(optsFrame, dropdown, 0, 0, 'LEFT');
+	dropdown:SetFrameLevel(optsFrame:GetFrameLevel() + 1);
+
+	dropdown.multi = multi;
+	dropdown.assoc = assoc;
+	dropdown.optsFrame = optsFrame;
+	dropdown.dropTex = dropTex;
+	dropdown.options = options;
+
+	function dropdown:ShowOptions()
+		for i = 1, #dropdowns do
+			dropdowns[i]:HideOptions();
+		end
+
+		self.optsFrame:UpdateSize(self:GetWidth(), self.optsFrame:GetHeight());
+		self.optsFrame:Show();
+		self.optsFrame:Update();
+		self:RepaintOptions();
+	end
+
+	function dropdown:HideOptions()
+		self.optsFrame:Hide();
+	end
+
+	function dropdown:ToggleOptions()
+		if self.optsFrame:IsShown() then
+			self:HideOptions();
+		else
+			self:ShowOptions();
+		end
+	end
+
+	function dropdown:SetPlaceholder(placeholderText)
+		if self:GetText() == '' or self:GetText() == self.placeholder then
+			self:SetText(placeholderText);
+		end
+
+		self.placeholder = placeholderText;
+	end
+
+	dropdown.buttonCreate = function(parent, i)
+		local optionButton;
+		if dropdown.multi then
+			optionButton = this:Checkbox(parent, '', parent:GetWidth(), 20);
+		else
+			optionButton = this:HighlightButton(parent, parent:GetWidth(), 20, '');
+			optionButton.text:SetJustifyH('LEFT');
+		end
+
+		optionButton.dropdown = dropdown;
+		optionButton:SetFrameLevel(parent:GetFrameLevel() + 2);
+		if not dropdown.multi then
+			optionButton:SetScript('OnClick', function(self)
+				self.dropdown:SetValue(self.value, self:GetText());
+				self.dropdown.optsFrame:Hide();
+			end);
+		else
+			optionButton.OnValueChanged = function(checkbox, isChecked)
+				checkbox.dropdown:ToggleValue(checkbox.value, isChecked);
+			end
+		end
+
+		return optionButton;
+	end
+
+	dropdown.buttonUpdate = function(parent, itemFrame, data)
+		itemFrame:SetWidth(parent:GetWidth());
+		itemFrame:SetText(data.text);
+
+		if itemFrame.dropdown.multi then
+			itemFrame:SetValue(data.value);
+		else
+			itemFrame.value = data.value;
+		end
+	end
+
+	function dropdown:RepaintOptions()
+		local scrollChild = self.optsFrame.scrollChild;
+		this:ObjectList(scrollChild, scrollChild.items, self.buttonCreate, self.buttonUpdate, self.options);
+		self.optsFrame:UpdateItemsCount(#self.options);
+	end
+
+	function dropdown:SetOptions(newOptions)
+		self.options = newOptions;
+		local optionsHeight = #newOptions * 20;
+		local scrollChild = self.optsFrame.scrollChild;
+
+		self.optsFrame:SetHeight(math.min(optionsHeight + 4, 200));
+		scrollChild:SetHeight(optionsHeight);
+
+		if not scrollChild.items then
+			scrollChild.items = {};
+		end
+
+		self:RepaintOptions();
+	end
+
+	function dropdown:ToggleValue(value, state)
+		assert(self.multi, 'Single dropdown cannot have more than one value!');
+
+		-- Treat is as associative array
+		if self.assoc then
+			self.value[value] = state;
+		else
+			if state then
+				-- we are toggling it on
+				if not tContains(self.value, value) then
+					tinsert(self.value, value);
+				end
+			else
+				-- we are removing it from table
+				if tContains(self.value, value) then
+					tDeleteItem(self.value, value);
+				end
+			end
+		end
+
+		self:SetValue(self.value);
+	end
+
+	function dropdown:SetValue(value, text)
+		self.value = value;
+
+		if text then
+			self:SetText(text);
+		else
+			self:SetText(self:FindValueText(value));
+		end
+
+		if self.multi then
+			for _, checkbox in pairs(self.optsFrame.scrollChild.items) do
+				local isChecked = self.assoc and
+					self.value[checkbox.value] or
+					tContains(self.value, checkbox.value);
+
+				checkbox:SetChecked(isChecked, true);
+			end
+		end
+
+		if self.OnValueChanged then
+			self.OnValueChanged(self, value, self:GetText());
+		end
+	end
+
+	function dropdown:GetValue()
+		return self.value;
+	end
+
+	function dropdown:FindValueText(value)
+		if type(value) ~= 'table' then
+			for i = 1, #self.options do
+				local opt = self.options[i];
+
+				if opt.value == value then
+					return opt.text;
+				end
+			end
+
+			return self.placeholder or '';
+		else
+			local result = '';
+
+			for i = 1, #self.options do
+				local opt = self.options[i];
+
+				if self.assoc then
+					for key, checked in pairs(value) do
+						if checked and key == opt.value then
+							if result == '' then
+								result = opt.text;
+							else
+								result = result .. ', ' .. opt.text;
+							end
+						end
+					end
+				else
+					for x = 1, #value do
+						if value[x] == opt.value then
+							if result == '' then
+								result = opt.text;
+							else
+								result = result .. ', ' .. opt.text;
+							end
+						end
+					end
+				end
+			end
+
+			if result ~= '' then
+				return result
+			else
+				return self.placeholder or '';
+			end
+		end
+	end
+
+	if options then
+		dropdown:SetOptions(options);
+	end
+
+	if value then
+		dropdown:SetValue(value);
+	elseif multi then
+		dropdown.value = {};
+	end
+
+	dropdown:SetScript('OnClick', function(self)
+		self:ToggleOptions();
+	end);
+
+	tinsert(dropdowns, dropdown);
+
+	return dropdown;
+end
+
+StdUi:RegisterModule(module, version);
