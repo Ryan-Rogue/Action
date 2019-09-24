1d7be129f037101a220b9faec0232cd70a7a2d5c
MisterCrab <51509374+MisterCrab@users.noreply.github.com>
2019-09-24 23:11:21 +0300

    Revert "Attemp to fix issue when Major Essence wiped after loading to mythic"
    This reverts commit 5e77388df81953da5be53d68bf66585ed0370686.

--- Modules/Dependences/AzeriteTraits.lua
+++ Modules/Dependences/AzeriteTraits.lua
@@ -33,6 +33,7 @@ local AzeriteEmpoweredItem 				= _G.C_AzeriteEmpoweredItem
 local AzeriteEssence 					= _G.C_AzeriteEssence
 
 local Data 								= {
+	TimeStamp							= 0,
 	InventorySlots 						= { 1, 2, 3, 5 },
 	Ranks 								= {},
 	Essences 							= {
@@ -271,14 +272,17 @@ end
 -------------------------------------------------------------------------------
 -- OnEvent
 -------------------------------------------------------------------------------
-local AzeriteItems 
 function Data.OnEvent()  	
-	if not AzeriteItems then 
-		for i = 1, #Data.InventorySlots do
-			AzeriteItems[Data.InventorySlots[i]] = Item:CreateFromEquipmentSlot(Data.InventorySlots[i])
-		end
-		A.Listener:Remove("ACTION_EVENT_AZERITE_TRAITS", "PLAYER_LOGIN")
+	-- Don't call it several times 
+	if Data.TimeStamp == TMW.time then 
+		return 
 	end 
+	Data.TimeStamp = TMW.time	
+	
+	local AzeriteItems = {}      	
+	for i = 1, #Data.InventorySlots do
+		AzeriteItems[Data.InventorySlots[i]] = Item:CreateFromEquipmentSlot(Data.InventorySlots[i])
+	end
 	
 	wipe(Data.Ranks)    
 	
@@ -323,7 +327,7 @@ if AzeriteEssence then
 	A.Listener:Add("ACTION_EVENT_AZERITE_TRAITS", "AZERITE_ESSENCE_CHANGED", 			Data.Essences.Update)
 	A.Listener:Add("ACTION_EVENT_AZERITE_TRAITS", "AZERITE_ESSENCE_UPDATE", 			Data.Essences.Update) 
 	A.Listener:Add("ACTION_EVENT_AZERITE_TRAITS", "AZERITE_ESSENCE_ACTIVATED", 			Data.Essences.Update)
-	--A.Listener:Add("ACTION_EVENT_AZERITE_TRAITS", "AZERITE_ESSENCE_ACTIVATION_FAILED", 	Data.Essences.Update)
+	A.Listener:Add("ACTION_EVENT_AZERITE_TRAITS", "AZERITE_ESSENCE_ACTIVATION_FAILED", 	Data.Essences.Update)
 end 
 
 -------------------------------------------------------------------------------
