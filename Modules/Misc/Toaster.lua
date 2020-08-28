f29658624e8eb2e0c6a364ef9dca771099765290
MisterCrab <51509374+MisterCrab@users.noreply.github.com>
2020-08-28 05:59:33 +0300

    [BETA] Added built-in Toaster (experimental)

---
+++ Modules/Misc/Toaster.lua
@@ -0,0 +1,25 @@
+-------------------------------------------------------------------------------------
+-- Toaster SavedVariables fix 
+-------------------------------------------------------------------------------------
+local _G, unpack 			= _G, unpack 
+local Toaster				= _G.Toaster
+local AceDB 				= _G.LibStub("AceDB-3.0")
+
+if Toaster and AceDB then 
+	local A 				= _G.Action
+	local CONST				= A.Const		
+	local wrongName			= CONST.ADDON_NAME .. "Settings"
+
+	local AceDBNew_Original = AceDB.New 
+	function AceDB:New(...)
+		local dbName = ...
+		if dbName == wrongName then 
+			local vararg = { ... }
+			vararg[1] = "ToasterSettings"
+			return AceDBNew_Original(self, unpack(vararg))
+		else 
+			return AceDBNew_Original(self, ...)
+		end 
+	end 
+end 
+
