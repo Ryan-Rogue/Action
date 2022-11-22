3b33c61899292144a61d90b434ec14cf0a3b9c9d
MisterCrab <51509374+MisterCrab@users.noreply.github.com>
2022-11-22 17:05:45 +0300

    Update Player.lua

--- Modules/Engines/Player.lua
+++ Modules/Engines/Player.lua
@@ -62,7 +62,7 @@ local 	 CancelUnitBuff, 	CancelSpellByName, 	  CombatLogGetCurrentEventInfo =
 	  
 -- Bags / Inventory
 local 	 C_Container = _G.C_Container
-local 	 GetContainerNumSlots, 	  									  GetContainerItemID, 	 								   GetInventoryItemID, 	GetItemInfoInstant,    GetItemCount, 	IsEquippableItem =	  
+local 	 GetContainerNumSlots, 	  									  GetContainerItemID, 	 								   GetInventoryItemID, 	  GetItemInfoInstant,    GetItemCount, 	  IsEquippableItem =	  
 	  _G.GetContainerNumSlots or C_Container.GetContainerNumSlots, _G.GetContainerItemID or C_Container.GetContainerItemID, _G.GetInventoryItemID, _G.GetItemInfoInstant, _G.GetItemCount, _G.IsEquippableItem	 
 	  
 -- Totems
