9cb118a8fb23593614c2db7c69499a2b12875a37
Ayni <51509374+MisterCrab@users.noreply.github.com>
2024-06-15 02:06:41 +0300

    Changed Unit(unitID):CanInterract() back to original

--- Action.lua
+++ Action.lua
@@ -1,5 +1,5 @@
 --- 
-local DateTime 														= "14.06.2024"
+local DateTime 														= "15.06.2024"
 ---
 local pcall, ipairs, pairs, type, assert, error, setfenv, getmetatable, setmetatable, loadstring, next, unpack, select, _G, coroutine, table, math, string =
 	  pcall, ipairs, pairs, type, assert, error, setfenv, getmetatable, setmetatable, loadstring, next, unpack, select, _G, coroutine, table, math, string
