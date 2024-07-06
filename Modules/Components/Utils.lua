9f963d17ab475e671f1b06885fe39abeb066c22e
Ayni <51509374+MisterCrab@users.noreply.github.com>
2024-07-06 03:20:23 +0300

    UniversalColor update for AutoProfile

--- Modules/Components/Utils.lua
+++ Modules/Components/Utils.lua
@@ -17,9 +17,73 @@ local toStr 					= A.toStr
 local toNum 					= A.toNum
 local Print 					= A.Print
 local ActionDataColor			= A.Data.C
+local ActionDataUniversalColor 	= A.Data.UC
 local BuildToC					= A.BuildToC
 
 local isClassic					= A.StdUi.isClassic
+local owner 					= isClassic and "PlayerClass" or "PlayerSpec" 
+local ownerColor				= ActionDataUniversalColor[0]
+local CharacterToUniversalColor	= {}
+do 
+	CharacterToUniversalColor[""] = ActionDataUniversalColor[0]
+	if isClassic then 		
+		CharacterToUniversalColor["WARRIOR"] = ActionDataUniversalColor[1]
+		CharacterToUniversalColor["PALADIN"] = ActionDataUniversalColor[2]
+		CharacterToUniversalColor["HUNTER"] = ActionDataUniversalColor[3]
+		CharacterToUniversalColor["ROGUE"] = ActionDataUniversalColor[4]
+		CharacterToUniversalColor["PRIEST"] = ActionDataUniversalColor[5]
+		CharacterToUniversalColor["SHAMAN"] = ActionDataUniversalColor[6]
+		CharacterToUniversalColor["MAGE"] = ActionDataUniversalColor[7]
+		CharacterToUniversalColor["WARLOCK"] = ActionDataUniversalColor[8]
+		CharacterToUniversalColor["MONK"] = ActionDataUniversalColor[9]
+		CharacterToUniversalColor["DRUID"] = ActionDataUniversalColor[10]
+		CharacterToUniversalColor["DEMONHUNTER"] = ActionDataUniversalColor[11]
+		CharacterToUniversalColor["DEATHKNIGHT"] = ActionDataUniversalColor[12]
+		CharacterToUniversalColor["EVOKER"] = ActionDataUniversalColor[13]		
+	else 
+		CharacterToUniversalColor[CONST.WARRIOR_ARMS] = ActionDataUniversalColor[1]	
+		CharacterToUniversalColor[CONST.WARRIOR_FURY] = ActionDataUniversalColor[2]		
+		CharacterToUniversalColor[CONST.WARRIOR_PROTECTION] = ActionDataUniversalColor[3]		
+		CharacterToUniversalColor[CONST.PALADIN_HOLY] = ActionDataUniversalColor[4]			
+		CharacterToUniversalColor[CONST.PALADIN_PROTECTION] = ActionDataUniversalColor[5]		
+		CharacterToUniversalColor[CONST.PALADIN_RETRIBUTION] = ActionDataUniversalColor[6]			
+		CharacterToUniversalColor[CONST.HUNTER_BEASTMASTERY] = ActionDataUniversalColor[7]	
+		CharacterToUniversalColor[CONST.HUNTER_MARKSMANSHIP] = ActionDataUniversalColor[8]			
+		CharacterToUniversalColor[CONST.HUNTER_SURVIVAL] = ActionDataUniversalColor[9]		
+		CharacterToUniversalColor[CONST.ROGUE_ASSASSINATION] = ActionDataUniversalColor[10]			
+		CharacterToUniversalColor[CONST.ROGUE_OUTLAW] = ActionDataUniversalColor[11]		
+		CharacterToUniversalColor[CONST.ROGUE_SUBTLETY] = ActionDataUniversalColor[12]			
+		CharacterToUniversalColor[CONST.PRIEST_DISCIPLINE] = ActionDataUniversalColor[13]			
+		CharacterToUniversalColor[CONST.PRIEST_HOLY] = ActionDataUniversalColor[14]			
+		CharacterToUniversalColor[CONST.PRIEST_SHADOW] = ActionDataUniversalColor[15]				
+		CharacterToUniversalColor[CONST.SHAMAN_ELEMENTAL] = ActionDataUniversalColor[16]			
+		CharacterToUniversalColor[CONST.SHAMAN_ENHANCEMENT] = ActionDataUniversalColor[17]	
+		CharacterToUniversalColor[CONST.SHAMAN_RESTORATION] = ActionDataUniversalColor[18]		
+		CharacterToUniversalColor[CONST.MAGE_ARCANE] = ActionDataUniversalColor[19]	
+		CharacterToUniversalColor[CONST.MAGE_FIRE] = ActionDataUniversalColor[20]	
+		CharacterToUniversalColor[CONST.MAGE_FROST] = ActionDataUniversalColor[21]		
+		CharacterToUniversalColor[CONST.WARLOCK_AFFLICTION] = ActionDataUniversalColor[22]			
+		CharacterToUniversalColor[CONST.WARLOCK_DEMONOLOGY] = ActionDataUniversalColor[23]			
+		CharacterToUniversalColor[CONST.WARLOCK_DESTRUCTION] = ActionDataUniversalColor[24]				
+		CharacterToUniversalColor[CONST.MONK_BREWMASTER] = ActionDataUniversalColor[25]	
+		CharacterToUniversalColor[CONST.MONK_MISTWEAVER] = ActionDataUniversalColor[26]	
+		CharacterToUniversalColor[CONST.MONK_WINDWALKER] = ActionDataUniversalColor[27]	
+		CharacterToUniversalColor[CONST.DRUID_BALANCE] = ActionDataUniversalColor[28]	
+		CharacterToUniversalColor[CONST.DRUID_FERAL] = ActionDataUniversalColor[29]	
+		CharacterToUniversalColor[CONST.DRUID_GUARDIAN] = ActionDataUniversalColor[30]	
+		CharacterToUniversalColor[CONST.DRUID_RESTORATION] = ActionDataUniversalColor[31]	
+		CharacterToUniversalColor[CONST.DEMONHUNTER_HAVOC] = ActionDataUniversalColor[32]	
+		CharacterToUniversalColor[CONST.DEMONHUNTER_VENGEANCE] = ActionDataUniversalColor[33]	
+		CharacterToUniversalColor[CONST.DEATHKNIGHT_BLOOD] = ActionDataUniversalColor[34]	
+		CharacterToUniversalColor[CONST.DEATHKNIGHT_FROST] = ActionDataUniversalColor[35]	
+		CharacterToUniversalColor[CONST.DEATHKNIGHT_UNHOLY] = ActionDataUniversalColor[36]	
+		CharacterToUniversalColor[CONST.EVOKER_DEVASTATION] = ActionDataUniversalColor[37]	
+		CharacterToUniversalColor[CONST.EVOKER_PRESERVATION] = ActionDataUniversalColor[38]	
+		CharacterToUniversalColor[CONST.EVOKER_AUGMENTATION] = ActionDataUniversalColor[39]			
+	end 
+	
+	ownerColor = CharacterToUniversalColor[A[owner] or ""]
+end 
 
 -------------------------------------------------------------------------------
 -- Remap
@@ -583,7 +647,7 @@ function Env.IsIconEnabled(icon)
 end
 
 -------------------------------------------------------------------------------
--- Scales
+-- Misc
 -------------------------------------------------------------------------------
 local BlackBackground = CreateFrame("Frame", nil, UIParent)
 if _G.BackdropTemplateMixin == nil and BlackBackground.SetBackdrop then -- Only expac less than Shadowlands
@@ -598,6 +662,29 @@ BlackBackground.texture = BlackBackground:CreateTexture(nil, "OVERLAY")
 BlackBackground.texture:SetAllPoints(true)
 BlackBackground.texture:SetColorTexture(0, 0, 0, 1)
 
+local function CreateMiscFrame(name, anchor, x, y)
+	local frame 		= CreateFrame("Frame", name, UIParent)
+	if frame.SetBackdrop then 
+		frame:SetBackdrop(nil)
+	end 
+	frame:SetFrameStrata("TOOLTIP")
+	frame:SetToplevel(true)
+	frame:SetSize(1, 1)
+	frame:SetScale(1)
+	frame:SetPoint(anchor, x, y)
+	frame.texture = frame:CreateTexture(nil, "OVERLAY")
+	frame.texture:SetAllPoints(true)
+	frame.texture:SetColorTexture(0, 0, 0, 1.0)
+	return frame
+end 
+
+local Character	 		 = CreateMiscFrame(nil, "TOPLEFT", 163, -3)
+Character.texture:SetColorTexture(ownerColor())
+TMW:RegisterCallback("TMW_ACTION_PLAYER_SPECIALIZATION_CHANGED", function()
+	ownerColor = CharacterToUniversalColor[A[owner]]
+	Character.texture:SetColorTexture(ownerColor())
+end) 
+
 local isShownOnce
 local function UpdateFrames()
     if not TellMeWhen_Group1 or not strfind(strlowerCache(TellMeWhen_Group1.Name), "shown main") then 
@@ -608,6 +695,10 @@ local function UpdateFrames()
         if TargetColor and TargetColor:IsShown() then
             TargetColor:Hide()
         end  
+		
+		if Character:IsShown() then 
+			Character:Hide()
+		end 		
 		
         return 
     end
@@ -654,6 +745,14 @@ local function UpdateFrames()
         end
         TargetColor:SetScale((0.71111112833023 * (1080 / myheight)) / (TargetColor:GetParent() and TargetColor:GetParent():GetEffectiveScale() or 1))
     end              
+
+	-- Character
+	if Character then 
+		if not Character:IsShown() then
+            Character:Show()
+        end
+        Character:SetScale((0.71111112833023 * (1080 / myheight)) / (Character:GetParent() and Character:GetParent():GetEffectiveScale() or 1))	
+	end 
 end
 
 local function UpdateCVAR()
