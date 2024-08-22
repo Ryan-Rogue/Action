dc5e49224cf2de49a7374bfd0e7ab0762d49f6fb
Ayni <51509374+MisterCrab@users.noreply.github.com>
2024-08-22 23:23:56 +0300

    Corrected FriendlyTeam for 5th unit-index

--- Modules/Engines/Unit.lua
+++ Modules/Engines/Unit.lua
@@ -4421,7 +4421,7 @@ A.FriendlyTeam = PseudoClass({
 				end 
 			end
 			
-			if TeamCacheFriendly.Type ~= "raid" then
+			if not TeamCacheFriendly.Type then
 				duration = A_Unit("player"):HasDeBuffs(spells) 
 				if duration ~= 0 then 
 					return duration, "player" 
@@ -4467,7 +4467,7 @@ A.FriendlyTeam = PseudoClass({
 				end 
 			end  
 			
-			if TeamCacheFriendly.Type ~= "raid" then
+			if not TeamCacheFriendly.Type then
 				duration = A_Unit("player"):HasBuffs(spells, source) 
 				if duration ~= 0 then 
 					return duration, "player" 
@@ -4546,7 +4546,7 @@ A.FriendlyTeam = PseudoClass({
 				end 
 			end  
 			
-			if TeamCacheFriendly.Type ~= "raid" then
+			if not TeamCacheFriendly.Type then
 				duration = A_Unit("player"):HasDeBuffs(spells) 
 				if duration ~= 0 then 
 					return duration, "player" 
@@ -4626,7 +4626,7 @@ A.FriendlyTeam = PseudoClass({
 				end                        
 			end  
 			
-			if TeamCacheFriendly.Type ~= "raid" and A_Unit("player"):TimeToDie() <= seconds then
+			if not TeamCacheFriendly.Type and A_Unit("player"):TimeToDie() <= seconds then
 				counter = counter + 1 
 				if counter >= count then 
 					return true, counter, "player"
@@ -4668,7 +4668,7 @@ A.FriendlyTeam = PseudoClass({
 				end                        
 			end  
 			
-			if TeamCacheFriendly.Type ~= "raid" then
+			if not TeamCacheFriendly.Type then
 				value = value + A_Unit("player"):TimeToDie()
 				members = members + 1
 			end 
@@ -4709,7 +4709,7 @@ A.FriendlyTeam = PseudoClass({
 				end                        
 			end
 
-			if TeamCacheFriendly.Type ~= "raid" and A_Unit("player"):HasBuffs(spells, source) == 0 then
+			if not TeamCacheFriendly.Type and A_Unit("player"):HasBuffs(spells, source) == 0 then
 				return true, "player"
 			end 	
 		end 		
@@ -4745,7 +4745,7 @@ A.FriendlyTeam = PseudoClass({
 				end 
 			end 
 
-			if TeamCacheFriendly.Type ~= "raid" and A_Unit("player"):CombatTime() > 0 and (not combatTime or A_Unit("player"):CombatTime() <= combatTime) then
+			if not TeamCacheFriendly.Type and A_Unit("player"):CombatTime() > 0 and (not combatTime or A_Unit("player"):CombatTime() <= combatTime) then
 				return true, "player"
 			end 	
 		end 
