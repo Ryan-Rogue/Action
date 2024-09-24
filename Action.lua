013850333d1d2690c13651596274081d91c82f8f
Ayni <51509374+MisterCrab@users.noreply.github.com>
2024-09-25 02:58:14 +0300

    Fix :5353 bad argument #1 to 'pairs'

--- Action.lua
+++ Action.lua
@@ -1,5 +1,5 @@
 --- 
-local DateTime 														= "22.09.2024"
+local DateTime 														= "25.09.2024"
 ---
 local pcall, ipairs, pairs, type, assert, error, setfenv, getmetatable, setmetatable, loadstring, next, unpack, select, _G, coroutine, table, math, string =
 	  pcall, ipairs, pairs, type, assert, error, setfenv, getmetatable, setmetatable, loadstring, next, unpack, select, _G, coroutine, table, math, string
@@ -5348,8 +5348,8 @@ local function tCompare(default, new, upkey, skip)
 	
 	if (new == nil or next(new) == nil) and default ~= nil then 
 		result = tMerge(result, default)		
-	else 		
-		if default ~= nil then 
+	else 				
+		if type(default) == "table" then 
 			for k, v in pairs(default) do
 				if not skip and new[k] ~= nil then 
 					if type(v) == "table" then 
