fdde6a8a565bef51e1035536bb9958525e41a049
Ayni <51509374+MisterCrab@users.noreply.github.com>
2025-04-05 03:07:21 +0300

    Updated TriggerGCD

--- Action.lua
+++ Action.lua
@@ -1,5 +1,5 @@
 --- 
-local DateTime 														= "03.04.2025"
+local DateTime 														= "05.04.2025"
 ---
 local pcall, ipairs, pairs, type, assert, error, setfenv, getmetatable, setmetatable, loadstring, next, unpack, select, _G, coroutine, table, math, string =
 	  pcall, ipairs, pairs, type, assert, error, setfenv, getmetatable, setmetatable, loadstring, next, unpack, select, _G, coroutine, table, math, string
