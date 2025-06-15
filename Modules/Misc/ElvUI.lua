ad29a322461bb4bf75e2a89a3481ff2ba3de33e4
Ayni <51509374+MisterCrab@users.noreply.github.com>
2025-06-15 16:49:54 +0300

    Added Cell addon support

--- Modules/Misc/ElvUI.lua
+++ Modules/Misc/ElvUI.lua
@@ -1,58 +1,76 @@
-local _G, getmetatable 					= _G, getmetatable
-
-local C_AddOns = _G.C_AddOns 
-local IsAddOnLoaded = C_AddOns and C_AddOns.IsAddOnLoaded or _G.IsAddOnLoaded
-if not _G.ElvUI and not IsAddOnLoaded("!InstanceSpellCollector") then 
-	return 
-end
-
-
+local _G, getmetatable, type			= _G, getmetatable, type
 local hooksecurefunc					= _G.hooksecurefunc	  
 local CreateFrame						= _G.CreateFrame	  
 local EnumerateFrames					= _G.EnumerateFrames
+local C_AddOns 							= _G.C_AddOns 
+local IsAddOnLoaded 					= C_AddOns and C_AddOns.IsAddOnLoaded or _G.IsAddOnLoaded
+
+local function IsLoadedPixelSnap()
+	if IsAddOnLoaded("ElvUI") or IsAddOnLoaded("Cell") then
+		return true
+	end
+end
 
 local handled = { ["Frame"] = true }
 local object = CreateFrame("Frame")
-object.t = object:CreateTexture(nil,"BACKGROUND")
-local OldTexelSnappingBias = object.t:GetTexelSnappingBias()
+object.texture = object:CreateTexture(nil, "BACKGROUND")
+local OldTexelSnappingBias = object.texture:GetTexelSnappingBias()
 
-local function Fix(frame)
+local function WatchPixelSnap(frame, snap)
+	if (frame and not frame:IsForbidden()) and not frame.PixelSnapDisabled and snap then
+		frame.PixelSnapTurnedOff = nil
+	end
+end
+
+local function EnablePixelSnap(frame)
 	if (frame and not frame:IsForbidden()) and frame.PixelSnapDisabled and not frame.PixelSnapTurnedOff then
 		if frame.SetSnapToPixelGrid then
 			frame:SetTexelSnappingBias(OldTexelSnappingBias)
 		elseif frame.GetStatusBarTexture then
 			local texture = frame:GetStatusBarTexture()
-			if texture and texture.SetSnapToPixelGrid then                
+			if type(texture) == 'table' and texture.SetSnapToPixelGrid then
 				texture:SetTexelSnappingBias(OldTexelSnappingBias)
 			end
 		end
+
 		frame.PixelSnapTurnedOff = true 
 	end
 end
 
-local function addapi(object)
+local function removeapi(object)
 	local mt = getmetatable(object).__index
 	if mt.DisabledPixelSnap then 
-		if mt.SetSnapToPixelGrid then hooksecurefunc(mt, 'SetSnapToPixelGrid', Fix) end
-		if mt.SetStatusBarTexture then hooksecurefunc(mt, 'SetStatusBarTexture', Fix) end
-		if mt.SetColorTexture then hooksecurefunc(mt, 'SetColorTexture', Fix) end
-		if mt.SetVertexColor then hooksecurefunc(mt, 'SetVertexColor', Fix) end
-		if mt.CreateTexture then hooksecurefunc(mt, 'CreateTexture', Fix) end
-		if mt.SetTexCoord then hooksecurefunc(mt, 'SetTexCoord', Fix) end
-		if mt.SetTexture then hooksecurefunc(mt, 'SetTexture', Fix) end
+		if mt.SetSnapToPixelGrid then hooksecurefunc(mt, 'SetSnapToPixelGrid', WatchPixelSnap) end
+		if mt.SetStatusBarTexture then hooksecurefunc(mt, 'SetStatusBarTexture', EnablePixelSnap) end
+		if mt.SetColorTexture then hooksecurefunc(mt, 'SetColorTexture', EnablePixelSnap) end
+		if mt.SetVertexColor then hooksecurefunc(mt, 'SetVertexColor', EnablePixelSnap) end
+		if mt.CreateTexture then hooksecurefunc(mt, 'CreateTexture', EnablePixelSnap) end
+		if mt.SetTexCoord then hooksecurefunc(mt, 'SetTexCoord', EnablePixelSnap) end
+		if mt.SetTexture then hooksecurefunc(mt, 'SetTexture', EnablePixelSnap) end
 	end
 end
 
-addapi(object)
-addapi(object:CreateTexture())
-addapi(object:CreateFontString())
-addapi(object:CreateMaskTexture())
-object = EnumerateFrames()
-while object do
-	if not object:IsForbidden() and not handled[object:GetObjectType()] then
-		addapi(object)
-		handled[object:GetObjectType()] = true
+object:RegisterEvent("PLAYER_LOGIN")
+object:SetScript("OnEvent", function(self, event)
+	if IsLoadedPixelSnap() then
+		removeapi(object)
+		removeapi(object.texture)
+		removeapi(object:CreateFontString())
+		removeapi(object:CreateMaskTexture())
+		removeapi(_G.GameFontNormal)
+
+		object = EnumerateFrames()
+		while object do
+			local objType = object:GetObjectType()
+			if not object:IsForbidden() and not handled[objType] then
+				removeapi(object)
+				handled[objType] = true
+			end
+			
+			object = EnumerateFrames(object)
+		end
 	end
 	
-	object = EnumerateFrames(object)
-end
+	self:UnregisterEvent(event)
+	self:SetScript("OnEvent", nil)
+end)
